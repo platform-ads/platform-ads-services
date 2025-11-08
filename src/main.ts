81fc90fe62daefce32ad6321ec81e1af5bb36d73
An Le Dai <ledaian22@gmail.com>
2025-11-08 15:49:45 +0700

    refactor: update import statements for bcrypt and cookie-parser to use namespace imports

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { GlobalExceptionFilter } from './filters/http-exception.filter';
 import { TransformResponseInterceptor } from './interceptors/transform-response.interceptor';
-import cookieParser from 'cookie-parser';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Cookie Parser Middleware
-  app.use(cookieParser());
+  app.use(cookieParser.default());
 
   // Global Exception Filter
   app.useGlobalFilters(new GlobalExceptionFilter());
