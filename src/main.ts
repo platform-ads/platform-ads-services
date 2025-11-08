9789a0083760b935a4a5099c1b954f20cdd522aa
An Le Dai <ledaian22@gmail.com>
2025-11-08 16:07:20 +0700

    fix: allow tsconfig.json in Docker build and fix cookie-parser import

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { GlobalExceptionFilter } from './filters/http-exception.filter';
 import { TransformResponseInterceptor } from './interceptors/transform-response.interceptor';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Cookie Parser Middleware
-  app.use(cookieParser.default());
+  app.use(cookieParser());
 
   // Global Exception Filter
   app.useGlobalFilters(new GlobalExceptionFilter());
